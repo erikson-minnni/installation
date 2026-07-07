f5392286a1413383eaded925d6cc5ba12830ba8d
erikson-minnni <111678710+erikson-minnni@users.noreply.github.com>
2026-07-07 19:25:40 +0200

    Create install_food_scale.sh

---
+++ install_food_scale.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+mkdir ~/food_scale
+cd ~/food_scale
+
+
+python3 -m venv --system-site-packages venv
+source venv/bin/activate
+
+pip install ultralytics
+pip install gspread google-auth flask
+
+sudo apt install python3-opencv
